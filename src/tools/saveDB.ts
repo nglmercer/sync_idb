3698711edfb6a25be6d4baf2fe5ed4ecbd1816c1
Tsurematsu <daniel120802@gmail.com>
2025-10-07 23:57:47 -0500

    eliminar saveDB (no se necesita)

--- src/tools/saveDB.ts
+++
@@ -1,9 +0,0 @@
-import { DataStorage } from 'json-obj-manager';
-import { JSONFileAdapter } from 'json-obj-manager/node';
-
-export default class saveDB{
-    
-    constructor(){
-        
-    }
-}
