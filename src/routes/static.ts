95b484925f022e71be2ab589c16eb5df0c070833
melserngl <128845117+nglmercer@users.noreply.github.com>
2025-10-13 23:47:22 -0500

    add create public if not exist publicPath

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -1,12 +1,14 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
 import * as path from 'path'
-
+import fs from 'fs'
 const staticRoutes = new Hono()
 const __dirname = process.cwd();
 // Configurar el directorio público para archivos estáticos
 const publicPath = path.join(__dirname, './public')
-
+if (!fs.existsSync(publicPath)) {
+  fs.mkdirSync(publicPath)
+}
 // Servir archivos estáticos desde el directorio public
 staticRoutes.use('/*', serveStatic({
   root: publicPath,
